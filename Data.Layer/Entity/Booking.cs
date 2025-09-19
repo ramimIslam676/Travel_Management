39c2b0138b647b0d61f7f7881ffdea6cd7e10ac9
ramimIslam676 <mdramimislam6332@gmail.com>
2025-09-19 19:31:10 +0600

    Add Booking, Payment, and Trip entity classes
    Introduced new entity classes for Booking, Payment, and Trip in the Data.Layer project. Updated the project file to include these classes for future data modeling and operations.

---
+++ Data.Layer/Entity/Booking.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Layer.Entity
+{
+    public class Booking
+    {
+        public int BookingId { get; set; }
+        public DateTime BookingDate { get; set; }
+        public int SeatBooked { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string PaymentStatus { get; set; }
+        public string BookingStatus { get; set; }
+        public int TripId { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
---
+++ Data.Layer/Entity/Trip.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Layer.Entity
+{
+    public class Trip
+    {
+        public int TripId { get; set; }
+        public string TripName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Destination { get; set; }
+        public decimal Price { get; set; }
+        public int TotalSeat { get; set; }
+        public int AvailableSeat { get; set; }
+        public string Description { get; set; }
+        public int OrganizerId { get; set; }
+    }
+}
