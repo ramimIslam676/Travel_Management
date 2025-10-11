01abda23be4e9638557f57f85ef17a43ed6f4783
ramimIslam676 <mdramimislam6332@gmail.com>
2025-10-12 00:46:19 +0600

    Add AdminPaymentForm and update AdminBookingForm UI
    Introduced AdminPaymentForm with designer, code-behind, and resource files for payment management. Updated AdminBookingForm.Designer.cs to enhance the UI layout and controls. Updated the project file to include the new form and its resources.

--- Final Project/AdminBookingForm.Designer.cs
+++ Final Project/AdminBookingForm.Designer.cs
@@ -33,6 +33,10 @@
             this.btnDelete = new System.Windows.Forms.Button();
             this.tbBookingId = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBookingId = new System.Windows.Forms.TextBox();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvBookingList)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,68 +44,85 @@
             // 
             this.dgvBookingList.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
             this.dgvBookingList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvBookingList.Location = new System.Drawing.Point(30, 21);
+            this.dgvBookingList.Location = new System.Drawing.Point(34, 26);
             this.dgvBookingList.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dgvBookingList.Name = "dgvBookingList";
             this.dgvBookingList.RowHeadersWidth = 62;
             this.dgvBookingList.RowTemplate.Height = 28;
-            this.dgvBookingList.Size = new System.Drawing.Size(447, 265);
+            this.dgvBookingList.Size = new System.Drawing.Size(503, 331);
             this.dgvBookingList.TabIndex = 0;
-            this.dgvBookingList.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvBookingList_CellContentDoubleClick);
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(674, 138);
+            this.btnCancel.Location = new System.Drawing.Point(0, 0);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(126, 42);
-            this.btnCancel.TabIndex = 1;
-            this.btnCancel.Text = "Cancel";
-            this.btnCancel.UseVisualStyleBackColor = true;
-            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 0;
             // 
             // btnDelete
             // 
-            this.btnDelete.Location = new System.Drawing.Point(674, 235);
+            this.btnDelete.Location = new System.Drawing.Point(0, 0);
             this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(126, 42);
-            this.btnDelete.TabIndex = 2;
-            this.btnDelete.Text = "Delete";
-            this.btnDelete.UseVisualStyleBackColor = true;
-            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 0;
             // 
             // tbBookingId
             // 
-            this.tbBookingId.Location = new System.Drawing.Point(739, 63);
+            this.tbBookingId.Location = new System.Drawing.Point(0, 0);
             this.tbBookingId.Name = "tbBookingId";
-            this.tbBookingId.Size = new System.Drawing.Size(137, 26);
-            this.tbBookingId.TabIndex = 3;
+            this.tbBookingId.Size = new System.Drawing.Size(100, 26);
+            this.tbBookingId.TabIndex = 0;
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(601, 68);
+            this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(88, 20);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Booking ID";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(689, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Booking ID";
+            // 
+            // txtBookingId
+            // 
+            this.txtBookingId.Location = new System.Drawing.Point(820, 76);
+            this.txtBookingId.Name = "txtBookingId";
+            this.txtBookingId.Size = new System.Drawing.Size(100, 26);
+            this.txtBookingId.TabIndex = 2;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(665, 197);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(112, 39);
+            this.buttonCancel.TabIndex = 3;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(845, 197);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(114, 39);
+            this.buttonDelete.TabIndex = 4;
+            this.buttonDelete.Text = "delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
             // 
             // AdminBookingForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-<<<<<<< Updated upstream
-            this.ClientSize = new System.Drawing.Size(817, 386);
-=======
-<<<<<<< HEAD
-            this.ClientSize = new System.Drawing.Size(919, 483);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.tbBookingId);
-            this.Controls.Add(this.btnDelete);
-            this.Controls.Add(this.btnCancel);
-=======
-            this.ClientSize = new System.Drawing.Size(817, 386);
->>>>>>> d1f25c33b922c1496b30a5fd341a8693a2347b67
->>>>>>> Stashed changes
+            this.ClientSize = new System.Drawing.Size(1034, 604);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.txtBookingId);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.dgvBookingList);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "AdminBookingForm";
@@ -120,5 +141,9 @@
         private System.Windows.Forms.Button btnDelete;
         private System.Windows.Forms.TextBox tbBookingId;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBookingId;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button buttonDelete;
     }
 }
---
+++ Final Project/AdminPaymentForm.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Final_Project
+{
+    partial class AdminPaymentForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvAdminPayment = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAdminPayment)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvAdminPayment
+            // 
+            this.dgvAdminPayment.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dgvAdminPayment.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAdminPayment.Location = new System.Drawing.Point(12, 12);
+            this.dgvAdminPayment.Name = "dgvAdminPayment";
+            this.dgvAdminPayment.RowHeadersWidth = 62;
+            this.dgvAdminPayment.RowTemplate.Height = 28;
+            this.dgvAdminPayment.Size = new System.Drawing.Size(432, 283);
+            this.dgvAdminPayment.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(655, 49);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(518, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Payment ID";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(571, 198);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 36);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Delete";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // AdminPaymentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dgvAdminPayment);
+            this.Name = "AdminPaymentForm";
+            this.Text = "AdminPaymentForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAdminPayment)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvAdminPayment;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
